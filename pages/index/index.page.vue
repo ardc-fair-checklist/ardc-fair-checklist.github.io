49688e7afba7c8127f442060d7382bda1b629b7a
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-11-02 19:43:58 +0100

    whitespace changes; switched import to use ~ alias; importing style sheet

--- pages/index/index.page.vue
+++ pages/index/index.page.vue
@@ -1,13 +1,14 @@
 <template>
-  <h1>Home</h1>
-  <p>
-    Choose the checklist for <Link v-bind:href="linkToDataChecklist">data</Link> or for <Link v-bind:href="linkToSoftwareChecklist">software</Link>.
-  </P>
+    <h1>Home</h1>
+    <p>
+      Choose the checklist for <Link v-bind:href="linkToDataChecklist">data</Link> or for <Link v-bind:href="linkToSoftwareChecklist">software</Link>.
+    </p>
+    <p>1936</p>
 </template>
 
-
 <script lang="ts" setup>
-import Link from './../../renderer/Link.vue'
+import Link from '~/renderer/Link.vue'
+import './style.css'
 const linkToDataChecklist = `${import.meta.env.BASE_URL}data/v0.2`
 const linkToSoftwareChecklist = `${import.meta.env.BASE_URL}software/v0.2`
 </script>
