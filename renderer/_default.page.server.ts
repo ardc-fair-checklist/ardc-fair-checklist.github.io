73aabf466ade3f37a258599d9df7f0fd4e0ccfbf
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-11-02 19:42:48 +0100

    now points to site logo favicon in public/

--- renderer/_default.page.server.ts
+++ renderer/_default.page.server.ts
@@ -1,7 +1,6 @@
 import { renderToString } from '@vue/server-renderer'
 import { escapeInject, dangerouslySkipEscape } from 'vite-plugin-ssr'
 import { createApp } from './app'
-import logoUrl from './logo.svg'
 import type { PageContextServer } from './types'
 
 export { render }
@@ -11,9 +10,7 @@ export const passToClient = [
     'effect',
     'pageProps',
     'render',
-    'urlOriginal',
-    'urlParsed',
-    'urlPathname'
+    'urlOriginal'
 ]
 
 async function render(pageContext: PageContextServer) {
@@ -29,7 +26,7 @@ async function render(pageContext: PageContextServer) {
         <html lang="en">
             <head>
                 <meta charset="UTF-8" />
-                <link rel="icon" href="${logoUrl}" />
+                <link rel="icon" href="/site-logo.svg" />
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta name="description" content="${desc}" />
                 <title>${title}</title>
