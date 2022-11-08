fa2e75f5da35a3238658736c69645a4124ccf582
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-11-08 17:11:55 +0100

    added new mechanism for redirecting

---
+++ pages/software/index.page.vue
@@ -0,0 +1,20 @@
+<template>
+    <p>
+        Redirecting...
+    </p>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { usePageContext } from '~/renderer/usePageContext'
+import '~/colors.css'
+import './style.css'
+
+onMounted(() => {
+    const { urlPathname } = usePageContext()
+    window.location.href = `/${urlPathname.split('/').filter(e => e !== "").join('/')}/v0.2`
+})
+</script>
+
+<style scoped>
+</style>
