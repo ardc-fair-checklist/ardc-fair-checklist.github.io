918cb156a93ccc7c633cb18adbe3806f2651020d
jspaaks <jspaaks@users.noreply.github.com>
2022-11-08 16:28:13 +0000

    Deploying to gh-pages from @ ardc-fair-checklist/ssg@f7c2583d490e37c3e3e26f8eb6be3fe95c1154ff 🚀

--- assets/chunk-806f2279.js
+++ assets/chunk-806f2279.js
@@ -814,53 +814,59 @@ const __vitePreload = function preload(baseModule, deps, importerUrl) {
     }
   })).then(() => baseModule());
 };
-const exportNames$d = ["render"];
+const exportNames$f = ["render"];
 const __vite_glob_2_0 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
   __proto__: null,
-  exportNames: exportNames$d
+  exportNames: exportNames$f
 }, Symbol.toStringTag, { value: "Module" }));
-const exportNames$c = ["render", "passToClient"];
+const exportNames$e = ["render", "passToClient"];
 const __vite_glob_3_0 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+  __proto__: null,
+  exportNames: exportNames$e
+}, Symbol.toStringTag, { value: "Module" }));
+const exportNames$d = ["default"];
+const exportNames$c = ["default", ...exportNames$d];
+const __vite_glob_4_0 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
   __proto__: null,
   exportNames: exportNames$c
 }, Symbol.toStringTag, { value: "Module" }));
 const exportNames$b = ["default"];
-const __vite_glob_4_0 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+const __vite_glob_4_1 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
   __proto__: null,
   exportNames: exportNames$b
 }, Symbol.toStringTag, { value: "Module" }));
 const exportNames$a = ["default"];
 const exportNames$9 = ["default", ...exportNames$a];
-const __vite_glob_4_1 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+const __vite_glob_4_2 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
   __proto__: null,
   exportNames: exportNames$9
 }, Symbol.toStringTag, { value: "Module" }));
 const exportNames$8 = ["default"];
 const exportNames$7 = ["default", ...exportNames$8];
-const __vite_glob_4_2 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+const __vite_glob_4_3 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
   __proto__: null,
   exportNames: exportNames$7
 }, Symbol.toStringTag, { value: "Module" }));
 const exportNames$6 = ["default"];
 const exportNames$5 = ["default", ...exportNames$6];
-const __vite_glob_4_3 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+const __vite_glob_4_4 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
   __proto__: null,
   exportNames: exportNames$5
 }, Symbol.toStringTag, { value: "Module" }));
 const exportNames$4 = ["default"];
-const __vite_glob_4_4 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+const __vite_glob_4_5 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
   __proto__: null,
   exportNames: exportNames$4
 }, Symbol.toStringTag, { value: "Module" }));
 const exportNames$3 = ["default"];
 const exportNames$2 = ["default", ...exportNames$3];
-const __vite_glob_4_5 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+const __vite_glob_4_6 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
   __proto__: null,
   exportNames: exportNames$2
 }, Symbol.toStringTag, { value: "Module" }));
 const exportNames$1 = ["default"];
 const exportNames = ["default", ...exportNames$1];
-const __vite_glob_4_6 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
+const __vite_glob_4_7 = /* @__PURE__ */ Object.freeze(/* @__PURE__ */ Object.defineProperty({
   __proto__: null,
   exportNames
 }, Symbol.toStringTag, { value: "Module" }));
@@ -1131,22 +1137,23 @@ function assertRenderHook(pageContext) {
   }
 }
 export {
-  getAllPageIdFilesServerSide as A,
-  releasePageContextCommon as B,
-  getPageContextSerializedInHtml as C,
-  loadPageFilesClientSide as D,
-  getErrorPageId as E,
-  getHook as F,
-  parse as G,
-  assertBaseUrl as H,
-  getPageFilesAll as I,
-  getPageFilesClientSide as J,
-  isParsable as K,
-  assertInfo as L,
-  isErrorFetchingStaticAssets as M,
-  assertHook as N,
-  assertRenderHook as O,
-  assertIsBundledOnce as P,
+  getCurrentUrl as A,
+  getAllPageIdFilesServerSide as B,
+  releasePageContextCommon as C,
+  getPageContextSerializedInHtml as D,
+  loadPageFilesClientSide as E,
+  getErrorPageId as F,
+  getHook as G,
+  parse as H,
+  assertBaseUrl as I,
+  getPageFilesAll as J,
+  getPageFilesClientSide as K,
+  isParsable as L,
+  assertInfo as M,
+  isErrorFetchingStaticAssets as N,
+  assertHook as O,
+  assertRenderHook as P,
+  assertIsBundledOnce as Q,
   __vitePreload as _,
   assert as a,
   isErrorPageId as b,
@@ -1169,9 +1176,9 @@ export {
   slice as s,
   __vite_glob_4_5 as t,
   __vite_glob_4_6 as u,
-  setPageFiles as v,
-  errorPrefix as w,
-  getProjectError as x,
-  getGlobalObject as y,
-  getCurrentUrl as z
+  __vite_glob_4_7 as v,
+  setPageFiles as w,
+  errorPrefix as x,
+  getProjectError as y,
+  getGlobalObject as z
 };
