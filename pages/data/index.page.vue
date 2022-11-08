f7c2583d490e37c3e3e26f8eb6be3fe95c1154ff
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-11-08 17:27:25 +0100

    redirection seems to work for software, copied mechanism to also redirect for data route

---
+++ pages/data/index.page.vue
@@ -0,0 +1,21 @@
+<template>
+    <p>
+        Redirecting...
+    </p>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { usePageContext } from '~/renderer/usePageContext'
+import '~/colors.css'
+import './style.css'
+
+onMounted(() => {
+    const { urlPathname } = usePageContext()
+    const url = `${import.meta.env.BASE_URL}${urlPathname}`
+    window.location.href = `/${url.split('/').filter(e => e !== "").join('/')}/v0.2`
+})
+</script>
+
+<style scoped>
+</style>
