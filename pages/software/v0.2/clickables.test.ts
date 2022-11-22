4aad2c69dc997ce809be688279d97f55076aec76
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-11-22 17:27:04 +0100

    converted testing prose to test suite stubs

---
+++ pages/software/v0.2/clickables.test.ts
@@ -0,0 +1,94 @@
+import { describe } from 'vitest'
+import { expect } from 'vitest'
+import { test } from 'vitest'
+
+describe('Clickables', () => {
+    describe('If the user clicks on the "Go to Badge" button,', () => {
+        test.skip('the app should scroll to the "Badges" section', () => {
+            expect(true).toBe(true);
+        })
+        test.skip('the app should disable the "Go to Badge" button', () => {
+            expect(true).toBe(true);
+        })
+    })
+    describe('If the user clicks on the "Home" button,', () => {
+        test.skip('the app should navigate to the home page', () => {
+            expect(true).toBe(true);
+        })
+    })
+    describe('If the user clicks on the "GitHub" button,', () => {
+        test.skip('the app should navigate to the "new issue" page of the repository on GitHub', () => {
+            expect(true).toBe(true);
+        })
+    })
+    describe('If the user clicks on the "data" link,', () => {
+        test.skip('the app should navigate to the latest version of the data app', () => {
+            expect(true).toBe(true);
+        })
+    })
+    describe('If a user clicks on a non-zero-points answer to a question,', () => {
+        test.skip('the progress percentage for the corresponding aspect should go up', () => {
+            expect(true).toBe(true);
+        })
+        test.skip('the progress percentage for the corresponding aspect should go up by the correct amount', () => {
+            expect(true).toBe(true);
+        })
+        test.skip('the overall progress percentage should go up', () => {
+            expect(true).toBe(true);
+        })
+        test.skip('the overall progress percentage should go up by the correct amount', () => {
+            expect(true).toBe(true);
+        })
+        test.skip('the answer that was clicked should get the \'selected\' state', () => {
+            expect(true).toBe(true);
+        })
+        test.skip('all other answers to that question should not have the \'selected\' state', () => {
+            expect(true).toBe(true);
+        })
+    })
+    describe('If a user has 100% on all aspects, and then clicks on a zero-points answer to a question,', () => {
+        test.skip('the progress percentage for the corresponding aspect should go down', () => {
+            expect(true).toBe(true);
+        })
+        test.skip('the progress percentage for the corresponding aspect should go down by the correct amount', () => {
+            expect(true).toBe(true);
+        })
+        test.skip('the overall progress percentage should go down', () => {
+            expect(true).toBe(true);
+        })
+        test.skip('the overall progress percentage should go down by the correct amount', () => {
+            expect(true).toBe(true);
+        })
+        test.skip('the answer that was clicked should get the \'selected\' state', () => {
+            expect(true).toBe(true);
+        })
+        test.skip('all other answers to that question should not have the \'selected\' state', () => {
+            expect(true).toBe(true);
+        })
+    })
+    describe('If a user clicks on the "Copy Markdown snippet" button,', () => {
+        test.skip('TODO', () => {
+            expect(true).toBe(true);
+        })
+    })
+    describe('If a user clicks on the "Copy ReStructured Text snippet" button,', () => {
+        test.skip('TODO', () => {
+            expect(true).toBe(true);
+        })
+    })
+    describe('If a user clicks on the "Copy HTML snippet" button,', () => {
+        test.skip('TODO', () => {
+            expect(true).toBe(true);
+        })
+    })
+    describe('If a user clicks on the eScience Center logo,', () => {
+        test.skip('the app should navigate to https://esciencecenter.nl', () => {
+            expect(true).toBe(true);
+        })
+    })
+    describe('If a user clicks on the ARDC logo,', () => {
+        test.skip('the app should navigate to https://ardc.edu.au', () => {
+            expect(true).toBe(true);
+        })
+    })
+})
---
+++ pages/software/v0.2/version-handling.test.ts
@@ -0,0 +1,11 @@
+import { describe } from 'vitest'
+import { expect } from 'vitest'
+import { test } from 'vitest'
+
+describe('Version handling', () => {
+    describe('If there is a newer version of the topic,', () => {
+        test.skip('the app should show a banner telling the user there is a newer version available', () => {
+            expect(true).toBe(true);
+        })
+    })
+})
