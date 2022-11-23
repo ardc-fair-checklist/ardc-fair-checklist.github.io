e2a98abecabad23cad79dd6b666d490a9dd84e08
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-11-23 19:11:15 +0100

    updated list of supported versions for data and for software

--- renderer/versions.ts
+++ renderer/versions.ts
@@ -1,12 +1,13 @@
-import { computed, ref } from 'vue';
+import { computed } from 'vue';
+import { ref } from 'vue';
 
 const state = ref({
-    software: ['v0.1', 'v0.2'],
-    data: ['v0.1', 'v0.2']
+    data: ['v0.1'],
+    software: ['v0.1']
 });
 
 export const versions = computed(() => state.value);
 export const latest = computed(() => ({
-    software: state.value.software.slice(-1)[0],
-    data: state.value.data.slice(-1)[0]
+    data: state.value.data.slice(-1)[0],
+    software: state.value.software.slice(-1)[0]
 }));
