dea29dc9abca49233ae09f5485340c8931d598b5
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-11-24 14:40:55 +0100

    fixed a few eslint things in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,19 @@
-import vue from '@vitejs/plugin-vue'
-import ssr from 'vite-plugin-ssr/plugin'
-import { UserConfig } from 'vite'
+import vue from '@vitejs/plugin-vue';
+import ssr from 'vite-plugin-ssr/plugin';
+import { UserConfig } from 'vite';
 
 const config: UserConfig = {
     base: '/ssg/',
-    //base: undefined,
+    // base: undefined,
     build: {
         minify: false,
         ssrManifest: true
     },
     plugins: [
         vue(),
-        ssr({
-            prerender: true
-        })
+        ssr({ prerender: true })
     ],
-    resolve: {
-        alias: {
-            "~": __dirname,
-        }
-    }
-}
+    resolve: { alias: { '~': __dirname } }
+};
 
-export default config
+export default config;
