d6c4db7eff6f060a306dc88c088bf680b6799ea9
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2022-11-29 21:11:37 +0100

    added test questions json; added units test for store; renamed a few files; removed export modifier from some variables in store because they were not used

--- pages/software/v0.1/BannerVersions.test.ts
+++ pages/software/v0.1/BannerVersions.test.ts
@@ -1,11 +1,11 @@
+import { assert } from 'vitest';
 import { describe } from 'vitest';
-import { expect } from 'vitest';
 import { test } from 'vitest';
 
 describe('Version handling', () => {
     describe('If there is a newer version of the topic,', () => {
         test.todo('the app should show a banner telling the user there is a newer version available', () => {
-            expect(true).toBe(true);
+            assert(true, 'apparently not true');
         });
     });
 });
