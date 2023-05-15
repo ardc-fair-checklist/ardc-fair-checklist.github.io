870e420f27758dd21880ea53b1f7f73ebba43a69
Jurriaan H. Spaaks <j.spaaks@esciencecenter.nl>
2023-05-15 12:22:58 +0200

    deleted data route and promoted contents of software route one level

--- pages/index/index.page.vue
+++ pages/index/index.page.vue
@@ -1,42 +1,26 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
-    <main>
-        <div>
-            <h1>FAIR checklists</h1>
-            <p>
-                Choose the checklist for
-                <ChecklistLink v-bind:href="linkToDataChecklist">data</ChecklistLink>
-                or for <ChecklistLink v-bind:href="linkToSoftwareChecklist">
-                software</ChecklistLink>.
-            </p>
-        </div>
-        <ChecklistFooter />
-    </main>
+    <p>
+        Redirecting...
+    </p>
 </template>
 
-<script lang="ts" setup>
+<script setup lang="ts">
+import { onMounted } from 'vue';
 import { latest } from '~/renderer/versions';
-import ChecklistFooter from './ChecklistFooter.vue';
-import ChecklistLink from './ChecklistLink.vue';
 import '~/renderer/global.css';
 import './app.css';
 
-const linkToDataChecklist = `${import.meta.env.BASE_URL}data/${latest.value.data}`;
-const linkToSoftwareChecklist = `${import.meta.env.BASE_URL}software/${latest.value.software}`;
+onMounted(() => {
+    const urlWithDuplicateSlashes = [
+        import.meta.env.BASE_URL,
+        'software',
+        latest.value.software
+    ].join('/');
+    const url = `/${urlWithDuplicateSlashes.split('/').filter(e => e !== '').join('/')}`;
+    window.location.href = url;
+});
 </script>
 
 <style scoped>
-main {
-    display: flex;
-    flex-direction: column;
-    flex-wrap: nowrap;
-    align-items: center;
-    justify-content: space-between;
-    height: 100vh;
-}
-h1 {
-    line-height: 1.2em;
-    margin-top: 3em;
-    margin-bottom: 3em;
-}
 </style>
